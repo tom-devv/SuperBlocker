9bc56567895915a7f219d244c514919420896bc0
Low-Lag <thomaspark.esq@gmail.com>
2022-06-27 19:53:10 +0100

    Removed useless depend check

--- src/main/java/dev/tom/Heart.java
+++ src/main/java/dev/tom/Heart.java
@@ -22,7 +22,6 @@ public class Heart extends JavaPlugin {
         this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Enabled Command Blocker");
         this.getServer().getPluginManager().registerEvents(new commandEvent(), this);
         config();
-        depends();
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
         manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.TAB_COMPLETE) {
             @Override
@@ -52,16 +51,6 @@ public class Heart extends JavaPlugin {
     public void config(){
         this.getConfig().options().copyDefaults();
         this.saveDefaultConfig();
-    }
-
-    public void depends(){
-        if(getServer().getPluginManager().getPlugin("ProtocolLib") != null){
-                System.out.println("Found ProtocolLib");
-        } else {
-            this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "ERROR CANNOT FIND PROTOCOL LIB");
-            this.getPluginLoader().disablePlugin(this);
-        }
-
     }
 
 }
